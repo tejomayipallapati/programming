2c1270defffecabf7595dfd974c2b5a71c4028d9
tejomayipallapati <pallapatitejomayi14@gmail.com>
2026-01-03 19:49:54 +0530

    hi

---
+++ python_programing/typecasting.py
@@ -0,0 +1,61 @@
+a=3
+print(a)
+print(type(a))
+
+a=str(3)
+print(type(a))
+
+a=8
+b=9
+print(a+b)
+print(type(a+b))
+
+a="teja"
+b="jashu"
+c="devi"
+print(a+" "+b+" "+c)
+
+num1=str(8)
+num2=str(9)
+print(num1+" "+num2)
+
+print(type(a+b+c),type(num1+num2))
+
+a=8
+print(a)
+print(type(str(a)))
+
+b=9
+print(b)
+print(type(float(b)))
+
+c="chandu"
+print(c)
+print(type(bool(c)))
+
+d=(
+"chandu""teja""sudhakar""devi""charan")
+print(d)
+print(type(list(d)))
+
+
+print("lets perform multipulation ")
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+print(type(a))
+print(type(b))
+c=a*b
+c=str(c)
+print("multipulation of a and b is "+c)
+
+
+print("lets perform addition ")
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+print(type(a))
+print(type(b))
+c=a+b
+c=str(c)
+print("addition of a and b is "+c)
+
+
